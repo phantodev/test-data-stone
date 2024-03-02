8a3c77017bd14b58a83f64ccdd5b02b84bc6736b
burkoelaborata <kduburko@gmail.com>
2024-03-02 11:21:56 -0300

    Layout Login finalizado Responsivo

--- components/login/LoginForm.vue
+++ components/login/LoginForm.vue
@@ -1,12 +1,21 @@
 <template>
   <section class="container-input">
-    <label class="label-input-text" for="email">E-mail</label>
+    <label class="label-input" for="email">E-mail</label>
     <input class="input-text" type="text" name="" id="email" />
   </section>
   <section class="container-input">
-    <label class="label-input-text" for="password">Senha</label>
+    <label class="label-input" for="password">Senha</label>
     <input class="input-text" type="text" name="" id="password" />
   </section>
+  <section class="container-input-checkbox">
+    <label class="container-check">
+      <section class="label-check">Lembrar-me</section>
+      <input type="checkbox" checked="checked" />
+      <span class="checkmark"></span>
+    </label>
+    <button class="btn-secondary">Esqueci Minha Senha</button>
+  </section>
+
   <button class="btn-primary" type="button">Entrar</button>
 </template>
 
@@ -17,5 +26,12 @@
   width: 100%;
   display: flex;
   flex-direction: column;
+}
+
+.container-input-checkbox {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,23 +15,33 @@
 </template>
 
 <style lang="scss">
-.login-form {
-  background-color: $white;
-  width: 30rem;
-  border-radius: 1rem;
-  padding: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  row-gap: 2rem;
-}
 .login-container {
   color: $primary;
-  background-color: $secondary;
+  background-color: $gray;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .login-form {
+    background-color: $white;
+    width: 30rem;
+    border-radius: 1rem;
+    padding: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    row-gap: 2rem;
+    box-shadow: 15px 15px 100px 15px rgba(0, 0, 0, 0.2);
+  }
+}
+@media (max-width: 420px) {
+  .login-container {
+    background-color: $white; // Altera a cor de fundo para branco em telas menores que 420px
+    .login-form {
+      box-shadow: none; // Remove a sombra em telas menores que 420px
+    }
+  }
 }
 </style>
