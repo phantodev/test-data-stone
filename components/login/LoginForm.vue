7a97ea7b20ff0bf91ede18a20b09fe8049f9f4fe
burkoelaborata <kduburko@gmail.com>
2024-03-02 10:06:22 -0300

    50% da estilização finalizada da tela de login. Falta Remember Check e Forgot

--- components/login/LoginForm.vue
+++ components/login/LoginForm.vue
@@ -1,7 +1,21 @@
 <template>
-  <section>Form Login</section>
+  <section class="container-input">
+    <label class="label-input-text" for="email">E-mail</label>
+    <input class="input-text" type="text" name="" id="email" />
+  </section>
+  <section class="container-input">
+    <label class="label-input-text" for="password">Senha</label>
+    <input class="input-text" type="text" name="" id="password" />
+  </section>
+  <button class="btn-primary" type="button">Entrar</button>
 </template>
 
 <script lang="ts"></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container-input {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
-<template>
-  <section>Login Page</section>
-</template>
-
 <script lang="ts"></script>
 
-<style lang="scss"></style>
+<template>
+  <section class="login-container">
+    <section class="login-form">
+      <section class="login-header">
+        <img
+          src="../assets/images/logo-data-stone.png"
+          alt="Logo da Empresa Data Stone"
+          width="284" />
+      </section>
+      <LoginForm />
+    </section>
+  </section>
+</template>
+
+<style lang="scss">
+.login-form {
+  background-color: $white;
+  width: 30rem;
+  border-radius: 1rem;
+  padding: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  row-gap: 2rem;
+}
+.login-container {
+  color: $primary;
+  background-color: $secondary;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
